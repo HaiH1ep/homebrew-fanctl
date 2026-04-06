7c8bc812c2a892f64caaec8a0b2fdffce9216406
Hai Hiep Nguyen <haihiep.ng291@gmail.com>
2026-04-06 12:14:38 +0700

    update v0.2.0

--- Formula/fanctl.rb
+++ Formula/fanctl.rb
@@ -1,7 +1,7 @@
 class Fanctl < Formula
   desc "Command-line tool for controlling Apple Silicon Mac fan speeds"
   homepage "https://github.com/haihiepnguyen/fanctl"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/haihiepnguyen/fanctl/releases/download/v#{version}/fanctl-v#{version}-arm64-macos.tar.gz"
   # Replace with actual sha256 after first release:
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
